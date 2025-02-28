8f3a005780bce02599a55e60479ab7a04b9aadd5
Pablo Olmedo Dorado <pablolmedorado@gmail.com>
2025-02-28 18:47:33 +0100

    build: initial configuration

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'coverage/**',
+        'dist/**',
+        '**/node_modules/**',
+        '**/*.d.ts',
+        '**/*.test.ts',
+        '**/*.config.ts',
+      ],
+      thresholds: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
+  },
+}) 
